7076e49b4821930781696e36fd5afbe387ff4d36
William Hernebrink <william.hernebrink@infor.com>
2022-01-21 16:31:23 +0100

    add lib with component

--- apps/site/src/app/app.module.ts
+++ apps/site/src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DesignSystemModule } from '@myworkspace/design-system'
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, DesignSystemModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
